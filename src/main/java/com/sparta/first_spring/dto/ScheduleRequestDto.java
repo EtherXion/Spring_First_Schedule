bfb7238a923e74fc9f4b7d6bc19dd9951aac665b
EtherXion <etel0103@gmail.com>
2024-08-14 18:03:36 +0900

    뭔가 잘못 건드린 것 같은데

---
+++ src/main/java/com/sparta/first_spring/dto/ScheduleRequestDto.java
@@ -0,0 +1,13 @@
+package com.sparta.first_spring.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ScheduleRequestDto {
+    private String todo;
+    private String manager;
+    private String password;
+    private String date;
+    private String time;
+
+}
